7490ad5969878f7372be82774a26f09103bbb336
scMarth <vlantaca@gmail.com>
2018-05-23 14:24:14 -0700

    update library with comments

---
+++ reAuxLib.py
@@ -0,0 +1,33 @@
+# Regular Expression Auxiliary Library
+
+import re
+
+'''
+
+Usage Example:
+
+from reAuxLib import stringContainsExpr, removeExtraSpaces
+
+removeExtraSpaces("   this is some    string   ")
+
+'''
+
+
+# returns a string that is just 'inputStr' with multi-spaces casted into single
+# spaces, and all whitespace before the first non-whitespace character removed,
+# as well as all whitespace after the last non-whitespace character removed
+def removeExtraSpaces(inputStr):
+   tokens = inputStr.upper().split()
+   returnStr = ""
+   for i in range(0, len(tokens)):
+      returnStr += tokens[i]
+      if (i != (len(tokens) - 1)):
+         returnStr += " "
+   return returnStr
+
+# returns true if the street 'street' is contained in the string 'names'
+def stringContainsExpr(expr, inputStr):
+   if re.search(expr, inputStr):
+      return True
+   return False
+
